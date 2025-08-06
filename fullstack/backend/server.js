9a72b08226afd6736fd05d56e43dbfc2de26e83c
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-08-06 14:41:02 +0530

    add front end and connect backend api through proxy

--- fullstack/backend/server.js
+++ fullstack/backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/jokes', (req, res) => {
+app.get('/api/jokes', (req, res) => {
   const jokes = [
     {
       id: 1,
