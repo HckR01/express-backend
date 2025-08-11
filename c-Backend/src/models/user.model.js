29dd224d678801dcddb8ab12954cf7384df3fc2b
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-08-11 20:44:22 +0530

    add models user and video

---
+++ c-Backend/src/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: {
+      type: Schema.Types.ObjectId,
+      ref: "video",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refereshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password"))
+    this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+export const User = mongoose.model("User", userSchema);
